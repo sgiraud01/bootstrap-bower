db431deb232fdba4ec306e3552e2bae6ed73164e
Steve Giraud <sgiraud01@gmail.com>
2015-09-28 11:49:07 +0200

    Update ui-bootstrap-tpls.js
    fix #2280 issue

--- ui-bootstrap-tpls.js
+++ ui-bootstrap-tpls.js
@@ -5609,7 +5609,7 @@ angular.module("template/modal/window.html", []).run(["$templateCache", function
     "<div modal-render=\"{{$isRendered}}\" tabindex=\"-1\" role=\"dialog\" class=\"modal\"\n" +
     "    modal-animation-class=\"fade\"\n" +
     "    modal-in-class=\"in\"\n" +
-    "	ng-style=\"{'z-index': 1050 + index*10, display: 'block'}\" ng-click=\"close($event)\">\n" +
+    "	ng-style=\"{'z-index': 1050 + index*10, display: 'block'}\">\n" +
     "    <div class=\"modal-dialog\" ng-class=\"size ? 'modal-' + size : ''\"><div class=\"modal-content\" modal-transclude></div></div>\n" +
     "</div>\n" +
     "");
